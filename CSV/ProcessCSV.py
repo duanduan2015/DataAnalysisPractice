88048633fe9c5267e9c7eafdeff9118c988005bb
Yunduan Han <yunduan.han@gmail.com>
2017-06-15 11:07:56 -0700

    Add new function to get unique students

--- CSV/ProcessCSV.py
+++ CSV/ProcessCSV.py
@@ -23,12 +23,16 @@ def parse_float(num):
     else:
         return float(num)
 
+def get_unique_students(records):
+    unique_students = set()
+    for student in records:
+        unique_students.add(student['account_key'])
+    return unique_students
+
 enrollments = read_csv('enrollments.csv')
 daily_engagement = read_csv('daily_engagement.csv')
 submissions = read_csv('project_submissions.csv')
 
-num_of_enrollments = len(enrollments)
-unique_enrollments = set()
 for enrollment in enrollments:
     enrollment['join_date'] = parse_date(enrollment['join_date'])
     enrollment['cancel_date'] = parse_date(enrollment['cancel_date'])
@@ -41,32 +45,31 @@ for enrollment in enrollments:
     else:
         enrollment['is_canceled'] = False
     enrollment['days_to_cancel'] = parse_int(enrollment['days_to_cancel'])
-    unique_enrollments.add(enrollment['account_key'])
-    #print(enrollment)
+num_of_enrollments = len(enrollments)
+unique_enrollments = get_unique_students(enrollments) 
 print('total number of enrollments is: ' + str(num_of_enrollments))
 print('unique number of enrollments is: ' + str(len(unique_enrollments)))
 
-num_of_engagements = len(daily_engagement)
-unique_engagements = set()
 for en in daily_engagement:
     en['utc_date'] = parse_date(en['utc_date'])
     en['num_courses_visited'] = parse_float(en['num_courses_visited'])
     en['total_minutes_visited'] = parse_float(en['total_minutes_visited'])
     en['lessons_completed'] = parse_float(en['lessons_completed'])
     en['projects_completed'] = parse_float(en['projects_completed'])
-    unique_engagements.add(en['acct'])
-    #print(en)
+    en['account_key'] = en['acct']
+    del en['acct']
+
+num_of_engagements = len(daily_engagement)
+unique_engagements = get_unique_students(daily_engagement) 
 print('total number of engagements is: ' + str(num_of_engagements))
 print('unique number of engagements is: ' + str(len(unique_engagements)))
 
-num_of_submissions = len(submissions)
-unique_submissions = set()
 for submission in submissions:
     submission['creation_date'] = parse_date(submission['creation_date'])
     submission['completion_date'] = parse_date(submission['completion_date'])
-    unique_submissions.add(submission['account_key'])
     #print(submission)
+num_of_submissions = len(submissions)
+unique_submissions = get_unique_students(submissions) 
 print('total number of submissions is: ' + str(num_of_submissions))
 print('unique number of submissions is: ' + str(len(unique_submissions)))
 
-    
