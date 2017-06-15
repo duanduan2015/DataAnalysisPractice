0f682a106322912b06ae6e983088d49fda654915
Yunduan Han <yunduan.han@gmail.com>
2017-06-14 21:14:07 -0700

    Add CSV practice in python

---
+++ CSV/ProcessCSV.py
@@ -0,0 +1,16 @@
+import unicodecsv
+def read_csv(filename):
+    with open(filename, 'rb') as f:
+        reader = unicodecsv.DictReader(f)
+        enrollments = list(reader)
+    return enrollments
+
+enrollments = read_csv('enrollments.csv')
+daily_engagement = read_csv('daily_engagement.csv')
+submissions = read_csv('project_submissions.csv')
+
+print(enrollments[0])
+print(daily_engagement[0])
+print(submissions[0])
+
+    
